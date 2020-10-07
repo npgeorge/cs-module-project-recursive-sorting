6f2f26726aa7ac15aa7fd7a2c59efc60c57cb9eb
npgeorge <npgeorge@users.noreply.github.com>
2020-10-06 23:01:31 -0400

    assignment

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,51 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    if start <= end:
+        mid = (start + end) // 2
+        
+        print("Searching list...")
+        print("start", start)
+        print("mid", mid)
+        print("end", end)
+        print("Middle index value of list: ", arr[mid])
+        
+        # return True if we find our target
+        if arr[mid] == target:
+            #return arr[target]
+            print("list: ", *arr)
+            return len(arr)
+
+        if arr[mid] < target:
+            print("Split towards end...")
+            print("list: ", arr[])
+            return binary_search(arr, target, mid + 1, end)
+        elif arr[mid] > target:
+            print("Split towards start...")
+            print("list: ", arr[:mid])
+            return binary_search(arr, target, start, mid - 1)
+    
+    return False
+
+#arr = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+#target = 5
+#start = 0
+#end = len(arr) - 1
+
+#print(binary_search(arr, target, start, end))
+
+#def contains(elements, value, left, right):
+#    if left <= right:
+#        middle = (left + right) // 2
+#
+#        if elements[middle] == value:
+#            return True
+#
+#        if elements[middle] < value:
+#            return contains(elements, value, middle + 1, right)
+#        elif elements[middle] > value:
+#            return contains(elements, value, left, middle - 1)
+#
+#    return False
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -11,4 +56,4 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    return
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,19 +1,74 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+    merged_arr = [0] * elements # what does this do for us?
+    # base case is when the number of elements = number of merged
+    # this is always true
+    #if elements == merged_arr:
+    #    return
 
-    # Your code here
+    #merge two sorted lists
+    #print("Elements: ", elements)
+    #print("Merged List: ", merged_arr)
+    #print(arrA + arrB)
 
+    # split list a into elements only
+    if arrA and arrB:
+        if arrA[0] > arrB[0]:
+            arrA, arrB = arrB, arrA
+        return [arrA[0]] + merge(arrA[1:], arrB)
 
-    return merged_arr
+    return arrA + arrB
+
+arrA = [0,1,2,3]
+arrB = [4,5,6,7]
+
+print(merge(arrA, arrB))
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    # Your code here
+    # if list is greater than 1
+    if len(arr) > 1:
+        #find mid
+        mid = len(arr) // 2
+        #left
+        left = arr[:mid]
+        #right
+        right = arr[mid:]
 
+        #sort left recursively
+        left = merge_sort(left)
+        #sort right recursively
+        right = merge_sort(right)
+
+        #create an empty list
+        arr = []
+
+        while len(left) > 0 and len(right) > 0:
+            if left[0] < right[0]:
+                arr.append(left[0])
+                left.pop(0)
+            else:
+                arr.append(right[0])
+                right.pop(0)
+        
+        for i in left:
+            arr.append(i)
+        for i in right:
+            arr.append(i)
 
     return arr
+
+# Input list 
+a = [12, 11, 13, 5, 6, 7] 
+print("Given array is") 
+print(*a) 
+  
+a = merge_sort(a) 
+  
+# Print output 
+print("Sorted array is : ") 
+print(*a) 
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
@@ -21,8 +76,10 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
+    pass
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
+    pass
 
